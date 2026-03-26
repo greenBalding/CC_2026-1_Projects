209f93d7583a33134565b79109ee948b30a32e55
greenBalding <matheuspains@pucgoias.edu.br>
2026-03-25 21:56:06 -0300

    fix: update retirement age calculation in coesão functionality comments

--- 2026.1-CMP2304/C01 - ARQUITETURA E DESENHO DE SOFTWARE/Atividade-de-coesao/atividade-de-coesao.c
+++ 2026.1-CMP2304/C01 - ARQUITETURA E DESENHO DE SOFTWARE/Atividade-de-coesao/atividade-de-coesao.c
@@ -9,6 +9,6 @@
 // A nossa função será uma função para calcular a idade de uma pessoa a partir do ano de nascimento e do ano atual.
 
 // ------ Coesão Sequencial: é quando a função tem mais de uma etapa e cada etapa depende da etapa anterior.
-// A nossa função será uma função que, a partir da idade calculada da pessoa, a gente descobre quanto tempo ainda falta para ela se aposentar, considerando uma idade de aposentadoria de 70 anos.
+// A nossa função será uma função que, a partir da idade calculada da pessoa, a gente descobre quanto tempo ainda falta para ela se aposentar, considerando uma idade de aposentadoria para homens de 65 anos e para mulheres de 62 anos.
 
 // ------ Main (chama as funções de coesão funcional e sequencial)
