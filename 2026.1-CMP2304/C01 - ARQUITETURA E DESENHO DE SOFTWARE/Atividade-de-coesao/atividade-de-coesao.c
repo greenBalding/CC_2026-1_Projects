3c621be595d82a58e75f67f24e19cfefe7661698
greenBalding <matheuspains@pucgoias.edu.br>
2026-03-25 21:49:15 -0300

    feat: add initial implementation of coesão functionality in C

---
+++ 2026.1-CMP2304/C01 - ARQUITETURA E DESENHO DE SOFTWARE/Atividade-de-coesao/atividade-de-coesao.c
@@ -0,0 +1,14 @@
+// ------ ALUNOS: 
+// MYLLENA RODRIGUES OLIVEIRA (20232002800012)
+// MATHEUS SILVA PAINS (20242002800320)
+
+// ------ ATIVIDADE:
+//Construir um código em C, módulos que implementem os tipos de coesão: Funcional e Sequencial.
+
+// ------ Coesão Funcional: é quando a função só tem uma finalidade.
+// A nossa função será uma função para calcular a idade de uma pessoa a partir do ano de nascimento e do ano atual.
+
+// ------ Coesão Sequencial: é quando a função tem mais de uma etapa e cada etapa depende da etapa anterior.
+// A nossa função será uma função que, a partir da idade calculada da pessoa, a gente descobre quanto tempo ainda falta para ela se aposentar, considerando uma idade de aposentadoria de 70 anos.
+
+// ------ Main (chama as funções de coesão funcional e sequencial)
