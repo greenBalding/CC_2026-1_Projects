658b96bc6f58f0b330d1a6f891a48e1dc9bb4e5d
greenBalding <matheuspains@pucgoias.edu.br>
2026-05-23 10:09:02 -0300

    chore(cleanup): remove .backup_frontend directories

--- 2026.1_-_Disciplinas/C01 - TECNOLOGIA DE CONSTRUÇÃO DE SOFTWARE I/LAB04_-_React/plataforma de cursos/.backup_frontend/src/models/trilha.model.tsx
+++
@@ -1,9 +0,0 @@
-import type { CategoriaModel } from './categoria.model.tsx';
-
-export interface TrilhaModel {
-  idTrilha: string;
-  titulo: string;
-  descricao: string;
-  idCategoria: string;
-  categoria?: CategoriaModel;
-}
